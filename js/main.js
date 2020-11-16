ef5159ec389d318ed0e016a9336164fb37b5655d
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2020-11-16 22:35:01 +0200

    15151

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover({trigger:'hover'});
-    
-});
+// (function() {
+//     'use strict';
+//     window.addEventListener('load', function() {
+//       var forms = document.getElementsByClassName('myForm');
+//       var validation = Array.prototype.filter.call(forms, function(form) {
+//         form.addEventListener('submit', function(event) {
+//           if (form.checkValidity() === false) {
+//             event.preventDefault();
+//             event.stopPropagation();
+//           }
+//           form.classList.add('was-validated');
+//         }, false);
+//       });
+//     }, false);
+//   })();
+
+
+
+$('.my-carousel').carousel({
+    interval: 4000
+  })
+  
