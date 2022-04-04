e80b52c1bfb92b543e41ca91bcf59a11b82351ce
Bakalina Yaroslav <bakalina.yaroslav@gmail.com>
2022-04-04 23:26:47 +0300

    created delete note

--- To Do list ( js and bootstrap )/scripts/main.js
+++ To Do list ( js and bootstrap )/scripts/main.js
@@ -11,14 +11,74 @@ let dataNotes = [
         select: "Idea",
         selectImage: "img/idea.png",
         text: "write a favorite book"
+    },
+    {
+        createDate: "2022-04-04",
+        date: "2022-04-23",
+        id: 1649102308223,
+        name: "shopping",
+        select: "Task",
+        selectImage: "img/task.png",
+        text: "foods"
+    },
+    {
+        createDate: "2022-04-04",
+        date: "2022-04-05",
+        id: 1649102963423,
+        name: "jogging",
+        select: "Idea",
+        selectImage: "img/idea.png",
+        text: "Start running in the morning"
+    },
+    {
+        createDate: "2022-04-04",
+        date: "2022-04-22",
+        id: 1649103122202,
+        name: "buy a laptop",
+        select: "Task",
+        selectImage: "img/task.png",
+        text: "go shopping"
+    },
+    {
+        createDate: "2022-04-04",
+        date: "",
+        id: 1649103312992,
+        name: "book",
+        select: "Quote",
+        selectImage: "img/quote.png",
+        text: "Sleep is good, he said, and books are better."
+    },
+    {
+        createDate: "2022-04-04",
+        date: "2022-04-04",
+        id: 1649103387474,
+        name: "food",
+        select: "Random Thought",
+        selectImage: "img/random.png",
+        text: "I want to eat"
+    },
+    {
+        createDate: "2022-04-04",
+        date: "",
+        id: 1649103547264,
+        name: "sleep",
+        select: "Random Thought",
+        selectImage: "img/random.png",
+        text: "i need to more sleep :)"
     }
 ];
 
+let clearNotesHtml = (selector) => {
+    let elementsNotes = document.querySelectorAll(selector)
+    elementsNotes.forEach(el => el.remove())
+}
+
 let renderRowActiveNotes = arrData => {
-    let divRow = document.createElement('div')
-    divRow.className = 'row align-items-center bg-light mt-2 rounded-3 form-control-sm'
-    arrData.map(el => {
-        return divRow.innerHTML = `<div class="col-1"><img alt="icon" style="max-width: 30px" src="${el.selectImage}"/></div>
+    clearNotesHtml('.activeNote')
+    arrData.forEach(el => {
+        let divRow = document.createElement('div')
+        divRow.className = 'row activeNote align-items-center bg-light mt-2 rounded-3 form-control-sm'
+        divRow.innerHTML = `<div class="col-1"><img alt="icon" style="max-width: 30px" src="${el.selectImage}"/></div>
                                    <div class="col-2 fw-bold">${slicedText(el.name)}</div>
                                    <div class="col-2">${el.createDate}</div>
                                    <div class="col-2">${el.select}</div>
@@ -28,12 +88,13 @@ let renderRowActiveNotes = arrData => {
                                            <div class="row justify-content-center ">
                                                   <div class="col-4 p-0"><img alt="img" src="img/edit.png"></div>
                                                   <div class="col-4 p-0"><img alt="img" src="img/archive.png"></div>
-                                                  <div class="col-4 p-0"><img alt="img" src="img/basket.png"></div>
+                                                  <div onclick="deleteNote(${el.id})" class="col-4 p-0"><img alt="img" src="img/basket.png"></div>
                                            </div>
                                    </div>`
+        document.querySelector('.elementsNotes').appendChild(divRow)
     })
 
-    document.querySelector('.buttonAddNote').before(divRow)
+
 };
 
 renderRowActiveNotes(dataNotes)
@@ -41,7 +102,6 @@ renderRowActiveNotes(dataNotes)
 function buttonAddNote() {
     document.getElementById('formData').style.display = 'block';
 }
-
 
 function addNotes(event) {
     event.preventDefault();
@@ -81,7 +141,6 @@ function addNotes(event) {
         document.getElementById('formData').style.display = '';
         renderRowActiveNotes(dataNotes)
     }
-    console.log(dataNotes)
 }
 
 function slicedText(text) {
@@ -92,6 +151,8 @@ function slicedText(text) {
     return sliced
 }
 
-
-
-
+function deleteNote(id) {
+    clearNotesHtml('.activeNote')
+    dataNotes = dataNotes.filter(el => el.id !== id)
+    renderRowActiveNotes(dataNotes)
+}
